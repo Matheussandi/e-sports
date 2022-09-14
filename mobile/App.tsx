a3feafaf902ac8b03c51aba1cc00c70934d832e6
Matheus <Matheussandi@hotmail.com>
2022-09-14 13:17:20 -0300

    :lipstick: feat: Interface Home e Loading

---
+++ mobile/App.tsx
@@ -0,0 +1,40 @@
+import { StatusBar } from 'react-native';
+
+import { Home } from './src/screens/Home' 
+import { Loading } from './src/components/Loading'
+
+import { 
+  useFonts ,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
+
+import { Background } from './src/components/Background';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
+  return (
+    <>
+      <Background>
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+
+        { fontsLoaded ? <Home /> : <Loading /> }
+
+      </Background>
+
+
+    </>
+  );
+}
---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Image, FlatList } from 'react-native';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { GameCard } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
+
+import { GAMES } from '../../utils/games';
+
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={logoImg}
+        style={styles.logo}
+      />
+
+      <Heading
+        title="Encontre sua dupla"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={item => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
+        renderItem={({ item }) => (
+          <GameCard
+            data={item}
+          />
+        )}
+      />
+    </View>
+  );
+}
