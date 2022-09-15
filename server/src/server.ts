43f96b75b1a059b5a6e11ae373d8bf3de60c811e
Matheus <Matheussandi@hotmail.com>
2022-09-15 18:04:51 -0300

    :wrench: Back-end construído

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,108 @@
 import express from 'express';
+import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
+
+
+import { convertHourStringToMinutes } from './utils/convertHourStringToMinutes';
+import { convertMinutesToHourString } from './utils/convertMinutesToHourString';
 
 const app = express();
+const prisma = new PrismaClient();
 
-app.get('/ads', (req, res) => {
-    return res.json([
-        { name: 'João', age: 24},
-        { name: 'Marcos', age: 18},
-        { name: 'Elias', age: 32},
-        { name: 'Maria', age: 26},
-    ])
+app.use(express.json());
+// configurar cors para definir quais endereços podem fazer requisição
+app.use(cors())
+
+app.get('/games', async (req, res) => {
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads: true,
+                }
+            }
+        }
+    })
+
+    return res.status(200).json(games);
+})
+
+app.post('/games/:id/ads', async (req, res) => {
+    const gameId = req.params.id;
+    const body = req.body;
+
+    try {
+        const ad = await prisma.ad.create({
+            data: {
+                gameId,
+                name: body.name,
+                weekDays: body.weekDays.join(','),
+                useVoiceChannel: body.useVoiceChannel,
+                yearsPlaying: body.yearsPlaying,
+                hourStart: convertHourStringToMinutes(body.hourStart),
+                hourEnd: convertHourStringToMinutes(body.hourEnd),
+                discord: body.discord,
+            }
+        });
+        return res.status(201).json(ad);
+    } catch (error) {
+        return res.status(400).json({ error: "Dados inválidos" });
+    }
+})
+
+app.get('/games/:id/ads', async (req, res) => {
+    const gameId = req.params.id;
+
+    // falta validar o ID do game
+    
+    try {
+        const ads = await prisma.ad.findMany({
+            select: {
+                id: true,
+                name: true,
+                weekDays: true,
+                useVoiceChannel: true,
+                yearsPlaying: true,
+                hourStart: true,
+                hourEnd: true,
+            },
+            where: {
+                gameId,
+            },
+            orderBy: {
+                createdAt: 'desc',
+            }
+        });
+    
+        return res.status(200).json(ads.map(ad => {
+            return {
+                ...ad,
+                weekDays: ad.weekDays.split(','),
+                hourStart: convertMinutesToHourString(ad.hourStart),
+                hourEnd: convertMinutesToHourString(ad.hourEnd),
+            }
+        }));
+    } catch (error) {
+        return res.status(400).json({ error: "Jogo inválido" });
+    }
+})
+
+app.get('/ads/:id/discord', async (req, res) => {
+    const adId = req.params.id;
+
+    // falta validar ID do ads
+
+    const ad = await prisma.ad.findUniqueOrThrow({
+        select: {
+            discord: true,
+        },
+        where: {
+            id: adId,
+        }
+
+    })
+
+    return res.status(200).json(ad);
 })
 
 app.listen('3333', () => {
